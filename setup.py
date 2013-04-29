acd3f8a64f424307882deafdde61be3643a54e33
Philip Sahli <ilipcito@gmail.com>
2013-04-29 17:36:35 +0200

    add pyyaml

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from distutils.core import setup
 setup(name='golive',
       version='0.1',
       py_modules=['golive'],
-      install_requires=['flup', 'Fabric'],
+      install_requires=['flup', 'Fabric', 'pyyaml'],
 )
