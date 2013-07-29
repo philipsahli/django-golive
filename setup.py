b6861db0eb199f1ae42267c0ec7a0a393fd04b4b
Philip Sahli <ilipcito@gmail.com>
2013-07-29 13:04:08 +0200

    add colorlog to setup

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from distutils.core import setup
 setup(name='golive',
       version='0.1',
       py_modules=['golive'],
-      install_requires=['flup', 'Fabric', 'pyyaml'],
+      install_requires=['flup', 'Fabric', 'pyyaml', 'colorlog'],
 )
