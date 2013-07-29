66fbf6ee26083a514be70cd5efc31d3365ba0a55
Philip Sahli <ilipcito@gmail.com>
2013-07-29 22:04:32 +0200

    db to postgres

--- project_examples/mezzanine_example/settings_sandbox.py
+++ project_examples/mezzanine_example/settings_sandbox.py
@@ -1,3 +1,9 @@
 from settings import *
 # STATIC_ROOT should be monkey-patched
 STATIC_ROOT = "/home/mezzanine_example_sandbox/static"
+
+DATABASES['default']['HOST'] = 'golive-sandbox3'
+DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+DATABASES['default']['NAME'] = 'mezzanine_example_sandbox'
+DATABASES['default']['USER'] = 'mezzanine_example_sandbox'
+DATABASES['default']['PASSWORD'] = os.environ['GOLIVE_DB_PASSWORD']
