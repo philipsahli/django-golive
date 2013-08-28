3b5ecb10d2b1c49ef0f2affa50e8d44bacf0495c
Philip Sahli <ilipcito@gmail.com>
2013-08-28 21:28:17 +0200

    remove gunicorn from wrong example

--- project_examples/django_example/django_example/settings.py
+++ project_examples/django_example/django_example/settings.py
@@ -126,7 +126,6 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
-    'gunicorn',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
