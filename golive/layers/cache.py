c4b58e2e43b29c507cd97aeb4e9a9cd4963ec68f
Philip Sahli <philip@sahli.net>
2014-02-22 17:23:39 +0100

    add roles to cache

--- golive/layers/cache.py
+++ golive/layers/cache.py
@@ -2,6 +2,6 @@ from golive.base import DjangoBaseTask, DebianPackageMixin
 
 
 class RedisSetup(DjangoBaseTask, DebianPackageMixin):
-    role = 'CACHE_HOST'
     # TODO: redis-server should be installed from squeeze-backports
     package_name = 'redis-server'
+    roles = 'CACHE_HOST'
